261b9d4c2c608674052bc1fac1d1d8b74155a41c
francis556 <vikrammehra244@gmail.com>
2022-06-12 18:27:23 +0200

    changed to dynamic post display

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,18 +4,7 @@
 <link rel="stylesheet" type="text/css" href="/app.css">
 <body>
     <article>
-        <h1><a href="/post">My First Post</a></h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <?= $post; ?>
     </article>
     <a href="/">Go Back</a>
 </body>
