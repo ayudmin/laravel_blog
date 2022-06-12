2f551a82625259d5eba2ae2bcbdba11a3a8e9d12
francis556 <vikrammehra244@gmail.com>
2022-06-12 18:28:37 +0200

    store blog posts as html files

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,14 @@ Route::get('/', function () {
 });
 
 
-Route::get('post', function () {
-    return view('post');
+Route::get('posts/{post}', function ($slug) {
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+    if (! file_exists($path)){
+        return redirect('');
+    }
+    $post = file_get_contents($path);
+    return view('post', [
+        'post' => $post
+
+    ]);
 });
