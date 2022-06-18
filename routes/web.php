8069ac2e1053bc3962c627d05c1d8c0ab1bc167e
francis556 <vikrammehra244@gmail.com>
2022-06-18 18:36:08 +0200

    route wildcat constraints

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,18 @@ Route::get('/', function () {
 
 
 Route::get('posts/{post}', function ($slug) {
+
     $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
     if (! file_exists($path)){
+
         return redirect('');
     }
+
     $post = file_get_contents($path);
+
     return view('post', [
         'post' => $post
 
     ]);
-});
+})->where('post', '[A-z_\-]+');
