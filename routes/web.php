f685c363e2fa99a139b5241bc87088a7c1a73d21
francis556 <vikrammehra244@gmail.com>
2022-06-19 06:10:01 +0200

    using catching for expensive operations

--- routes/web.php
+++ routes/web.php
@@ -20,17 +20,21 @@ Route::get('/', function () {
 
 Route::get('posts/{post}', function ($slug) {
 
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+    if (! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html";)){
 
-    if (! file_exists($path)){
-
-        return redirect('');
+        return redirect('/');
     }
 
-    $post = file_get_contents($path);
+    $post = cache()->remember("posts.{$slug}", 5, function () use ($path) {
+
+        return file_get_contents($path);
+
+    });
+
 
     return view('post', [
         'post' => $post
 
     ]);
+    
 })->where('post', '[A-z_\-]+');
